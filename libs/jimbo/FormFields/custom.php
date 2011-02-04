2de36ea9eebdd05c03c3416e973881a8a0f9242a
User <star@power.young.org.ua>
2011-02-04 09:46:03 +0200

    commit

--- libs/jimbo/FormFields/custom.php
+++ libs/jimbo/FormFields/custom.php
@@ -15,8 +15,24 @@ class cityFormField extends abstractFormField {
 		$out .= '</select>';
 		return $out;
 	}
+}
 
+class doubleVisitGeoRegionFormField extends abstractFormField {
 
+	function getEditInput($value = '') {
+		global $db, $_sessionData;
+		
+		$city = $db->getCol('SELECT distinct(geo_region) FROM double_visit WHERE slave_worker_id = '.$_sessionData['auth_id'].' ORDER BY geo_region');
+		
+		$value = htmlspecialchars(stripslashes($value));
+		$out = '<input style="width:150px" type="text" name="'.$this->name.'" id="'.$this->name.'" value="'.$value.'" class="thin">';		
+		$out .= '&nbsp;<select style="width:190px" class="thin" onChange="doSelectTo(this, \''.$this->name.'\')">';
+		foreach ($city as $item) {
+			$out .= '<option value="'.htmlspecialchars($item).'">'.htmlspecialchars($item);
+		}
+		$out .= '</select>';
+		return $out;
+	}
 }
 
 class texthintFormField extends abstractFormField {
--- plugins/TblDefinitionPlugin.php
+++ plugins/TblDefinitionPlugin.php
@@ -2,7 +2,13 @@
 class TblDefinitionPlugin extends Plugin
 {
 	public function definition(&$tpl)
-	{
+	{        
+        $info = array(
+        	'role' => $GLOBALS['_sessionData']['auth_role'],
+        	'position' => $GLOBALS['_sessionData']['position_id']
+        );
+        
+        $tpl->assign('info', $info);
 	}
 }
 ?>
