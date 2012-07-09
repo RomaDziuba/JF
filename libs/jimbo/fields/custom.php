72a4f74715f27c858c7c1e8185f7b6315d556c4e
Goliath <goliathdp@gmail.com>
2012-07-09 22:56:17 +0300

    Fix wysiwygFormField

--- libs/jimbo/fields/custom.php
+++ libs/jimbo/fields/custom.php
@@ -21,11 +21,11 @@ class doubleVisitGeoRegionFormField extends abstractFormField {
 
 	function getEditInput($value = '', $inline = false) {
 		global $db, $_sessionData;
-		
+
 		$city = $db->getCol('SELECT distinct(geo_region) FROM double_visit WHERE slave_worker_id = '.$_sessionData['auth_id'].' ORDER BY geo_region');
-		
+
 		$value = htmlspecialchars(stripslashes($value));
-		$out = '<input style="width:150px" type="text" name="'.$this->name.'" id="'.$this->name.'" value="'.$value.'" class="thin">';		
+		$out = '<input style="width:150px" type="text" name="'.$this->name.'" id="'.$this->name.'" value="'.$value.'" class="thin">';
 		$out .= '&nbsp;<select style="width:190px" class="thin" onChange="doSelectTo(this, \''.$this->name.'\')"><option value=""></option>';
 		foreach ($city as $item) {
 			$out .= '<option value="'.htmlspecialchars($item).'">'.htmlspecialchars($item);
@@ -39,24 +39,24 @@ class healthFacilityAddrStreetFormField extends abstractFormField {
 
 	function getEditInput($value = '', $inline = false) {
 		global $db, $_sessionData;
-		
+
 		$ID = (int)$_GET['ID'];
 		if (!empty($ID)) {
-			$hfCity = (int)$db->getOne('SELECT city_id FROM health_facility WHERE id='.$ID);		
+			$hfCity = (int)$db->getOne('SELECT city_id FROM health_facility WHERE id='.$ID);
 			$streets = $db->getCol('SELECT DISTINCT(addr_street) FROM health_facility WHERE city_id = '.$hfCity.' ORDER BY addr_street');
 		}
 		else {
 			$streets = $db->getCol('SELECT DISTINCT(addr_street) FROM health_facility ORDER BY addr_street');
 		}
-		
+
 		$value = htmlspecialchars(stripslashes($value));
-		$out = '<input style="width:150px" type="text" name="'.$this->name.'" id="'.$this->name.'" value="'.$value.'" class="thin">';		
+		$out = '<input style="width:150px" type="text" name="'.$this->name.'" id="'.$this->name.'" value="'.$value.'" class="thin">';
 		$out .= '&nbsp;<select style="width:190px" class="thin" onChange="doSelectTo(this, \''.$this->name.'\')"><option value=""></option>';
 		foreach ($streets as $item) {
 			$out .= '<option value="'.htmlspecialchars($item).'">'.htmlspecialchars($item);
 		}
 		$out .= '</select>';
-		
+
 		return $out;
 	}
 }
@@ -65,30 +65,30 @@ class shopAddrStreetFormField extends abstractFormField {
 
 	function getEditInput($value = '', $inline = false) {
 		global $db, $_sessionData;
-		
+
 		$ID = (int)$_GET['ID'];
 		if (!empty($ID)) {
-			$hfCity = (int)$db->getOne('SELECT city_id FROM shop WHERE id='.$ID);		
+			$hfCity = (int)$db->getOne('SELECT city_id FROM shop WHERE id='.$ID);
 			$streets = $db->getCol('SELECT DISTINCT(addr_street) FROM shop WHERE city_id = '.$hfCity.' ORDER BY addr_street');
 		}
 		else {
 			$streets = $db->getCol('SELECT DISTINCT(addr_street) FROM shop ORDER BY addr_street');
 		}
-		
+
 		$value = htmlspecialchars(stripslashes($value));
-		$out = '<input style="width:150px" type="text" name="'.$this->name.'" id="'.$this->name.'" value="'.$value.'" class="thin">';		
+		$out = '<input style="width:150px" type="text" name="'.$this->name.'" id="'.$this->name.'" value="'.$value.'" class="thin">';
 		$out .= '&nbsp;<select style="width:190px" class="thin" onChange="doSelectTo(this, \''.$this->name.'\')"><option value=""></option>';
 		foreach ($streets as $item) {
 			$out .= '<option value="'.htmlspecialchars($item).'">'.htmlspecialchars($item);
 		}
 		$out .= '</select>';
-		
+
 		return $out;
 	}
 }
 
 class autocompleteFormField extends abstractFormField {
-	
+
 	function getEditInput($value = '', $inline = false) {
 		$value = htmlspecialchars($value);
 		$readonly = $this->getAttribute('readonly') == 'true' ? 'readonly' : '';
@@ -99,16 +99,16 @@ class autocompleteFormField extends abstractFormField {
 								jQuery('#".$this->name."_autocomplete').autocomplete({
 								    serviceUrl: '".$this->attributes['autocompleteURL']."',
 								    minChars: ".$this->attributes['minChars'].",
-								    maxHeight: 400, 
-								    width: 380, 
+								    maxHeight: 400,
+								    width: 380,
 								    zIndex: 9999,
-								    deferRequestBy: 300 
+								    deferRequestBy: 300
 								});
 					})(jQuery)
 				</script>";
 		return $out;
 	}
-	
+
 }
 
 class texthintFormField extends abstractFormField {
@@ -129,17 +129,17 @@ class texthintFormField extends abstractFormField {
 	}
 }
 
-class wysiwygFormField extends  abstractFormField 
+class wysiwygFormField extends  abstractFormField
 {
 
-    function getEditInput($value = '') 
+    function getEditInput($value = '', $inline = false)
     {
         global $jimbo;
-        
-        
-        
+
+
+
         $httpBase = $jimbo->getOption("http_base");
-        
+
         //$jimbo->includeJs($httpBase.'js/base.js');
         $value = htmlspecialchars($value, ENT_COMPAT, 'utf-8');
         $out = <<< END
